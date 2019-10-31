b81039bc1168ce93f5b33aa5c63cd5e2a5514884
pisenliang <pisenliang@gmail.com>
2019-10-31 09:31:27 +0800

    amx 1.x 1.3.3版本

--- src/lib/index.js
+++ src/lib/index.js
@@ -4,79 +4,9 @@
  * @Email: pisenliang@gmail.com
  * @Date: 2019-06-17 15:37:41
  * @LastEditors: PiPi
- * @LastEditTime: 2019-10-09 13:58:36
+ * @LastEditTime: 2019-10-31 09:29:13
  */
-function recursion(obj,Callback){
-  var num = {}
-  function x(o,z) {
-    for(let a in o){
-      let value = o[a],voType
-      Object.defineProperty( z, a, {
-        enumerable: true,
-        configurable: true,
-        get: function() {
-          return value
-        },
-        set: function(v) {
-          value = v
-          Callback()
-        }
-      })
-      try{
-        voType = request[o].constructor.name
-      }catch (e){
-        voType = ''
-      }
-      if(voType == 'Object'){
-        x(o[a],z[a])
-      }
-    }
-  }
-  x(obj,num)
-  return num
-}
-function keyData(key){
-  var request = {}
-  try{
-    request[key] = JSON.parse(dataGet(key))
-  }catch (e){
-    request[key] = dataGet(key)
-  }
-  for(let o in request){
-    let value = request[o],voType
-    try{
-      voType = request[o].constructor.name
-    }catch (e){
-      voType = ''
-    }
-    if(voType == 'Object'){
-      value = recursion(request[o],function(){
-        if (localStorage.getItem(o)) {
-          localStorage.setItem(o, JSON.stringify(window.amx[o]))
-        }else if(sessionStorage.getItem(todo)){
-          sessionStorage.setItem(o, JSON.stringify(window.amx[o]))
-        }
-      })
-    }
-    
-    Object.defineProperty(window.amx, o, {
-      enumerable: true,
-      configurable: true,
-      get: function() {
-        return value
-      },
-      set: function(v) {
-        value = v
-        if (localStorage.getItem(o)) {
-          localStorage.setItem(o, JSON.stringify(v))
-        }else if(sessionStorage.getItem(todo)){
-          sessionStorage.setItem(o, JSON.stringify(v))
-        }
-      }
-    })
-  }
-  // return back
-}
+// localStorage 写入操作 监听
 let orignalSetItem = localStorage.setItem;
 localStorage.setItem = function (key, newValue) {
   let setItemEvent = new Event("setItemEvent")
@@ -85,6 +15,108 @@ localStorage.setItem = function (key, newValue) {
   window.dispatchEvent(setItemEvent)
   orignalSetItem.apply(this, arguments)
 }
+//监听 localStorage 改变
+window.addEventListener('storage', (e) => {
+  if(window.amx[e.key] != e.newValue)
+  try {
+    window.amx[e.key] = recursion(JSON.parse(e.newValue),function () {
+      if (localStorage.getItem(e.key)) {
+        localStorage.setItem(e.key, JSON.stringify(window.amx[e.key]))
+      } else if (sessionStorage.getItem(todo)) {
+        sessionStorage.setItem(e.key, JSON.stringify(window.amx[e.key]))
+      }
+    })
+  } catch (e) {
+    window.amx[e.key] = e.newValue
+  }
+})
+//监听 localStorage 写入
+window.addEventListener("setItemEvent", (e) => {
+  // console.log(e)
+  if(JSON.stringify(window.amx[e.key]) != e.newValue){
+    try {
+      window.amx[e.key] = recursion(JSON.parse(e.newValue),function () {
+        if (localStorage.getItem(e.key)) {
+          localStorage.setItem(e.key, JSON.stringify(window.amx[e.key]))
+        } else if (sessionStorage.getItem(todo)) {
+          sessionStorage.setItem(e.key, JSON.stringify(window.amx[e.key]))
+        }
+      })
+    } catch (e) {
+      window.amx[e.key] = e.newValue
+    }
+  }
+});
+
+// 深度监听 json
+function recursion(obj, Callback) {
+  var num = {}
+  function x(o, z) {
+    for (let a in o) {
+      let value = o[a], voType
+      Object.defineProperty(z, a, {
+        enumerable: true,
+        configurable: true,
+        get: function () {
+          return value
+        },
+        set: function (v) {
+          value = v
+          Callback()
+        }
+      })
+      try {
+        voType = request[o].constructor.name
+      } catch (e) {
+        voType = ''
+      }
+      if (voType == 'Object') {
+        x(o[a], z[a])
+      }
+    }
+  }
+  x(obj, num)
+  return num
+}
+// 深度监听一个 localStorage
+function keyData(key) {
+  var value = {}, voType
+  try {
+    value[key] = JSON.parse(dataGet(key))
+  } catch (e) {
+    value[key] = dataGet(key)
+  }
+  try {
+    voType = value[key].constructor.name
+  } catch (e) {
+    voType = ''
+  }
+  if (voType == 'Object') {
+    value = recursion(value[key], function () {
+      if (localStorage.getItem(key)) {
+        localStorage.setItem(key, JSON.stringify(window.amx[key]))
+      } else if (sessionStorage.getItem(todo)) {
+        sessionStorage.setItem(key, JSON.stringify(window.amx[key]))
+      }
+    })
+  }
+  Object.defineProperty(window.amx, key, {
+    enumerable: true,
+    configurable: true,
+    get: function () {
+      return value
+    },
+    set: function (v) {
+      value = v
+      if (localStorage.getItem(key)) {
+        localStorage.setItem(key, JSON.stringify(v))
+      } else if (sessionStorage.getItem(todo)) {
+        sessionStorage.setItem(key, JSON.stringify(v))
+      }
+    }
+  })
+}
+
 function dataGet(key) {
   const type1 = localStorage.getItem(key), type2 = sessionStorage.getItem(key)
   if (!type1 && !type2) {
@@ -105,7 +137,7 @@ function dataGet(key) {
 }
 let Toast = {}
 Toast.install = function (Vue, todos) {
-  var timestamp=new Date().getTime()
+  var timestamp = new Date().getTime()
   let result = {}
   if (todos.data) {
     for (let todo in todos.data) {
@@ -122,51 +154,27 @@ Toast.install = function (Vue, todos) {
     }
   }
   window.amx = {}
-  // console.log((new Date().getTime()-timestamp))
 }
 
 Toast.read = function (key) {
-  // var timestamp=new Date().getTime()
-  const type1 = localStorage.getItem(key), type2 = sessionStorage.getItem(key)
   let data = {}
-  if(!window.amx[key]){
+  if (!window.amx[key]) {
     keyData(key)
   }
-  data[key] = window.amx[key]
-  let watch = {}
-  watch[key] = {
-    handler(val, oldVal) {
-      if (type1) {
-        localStorage.setItem(key, JSON.stringify(val))
-      } else {
-        sessionStorage.setItem(key, JSON.stringify(val))
-      }
-    },
-    deep: true
-  }
-  // console.log((new Date().getTime()-timestamp))
   return {
     data() {
-      return data
+      return window.amx
     },
-    created(){
+    created() {
       this[key] = window.amx[key]
     },
     mounted() {
       window.addEventListener('storage', (e) => {
         if (e.key == key) {
-          this[key] = JSON.parse(e.newValue)
+          this[key] = window.amx[key]
         }
       })
-      window.addEventListener("setItemEvent", (e) => {
-        if (e.key == key) {
-          if (e.newValue != dataGet(key)) {
-            this[key] = JSON.parse(e.newValue)
-          }
-        }
-      });
-    },
-    // watch: watch
+    }
   }
 }
 
@@ -186,53 +194,5 @@ Toast.allDelete = function (type) {
   }
   sessionStorage.clear()
   localStorage.clear()
-}
-Toast.allData = function () {
-  let storage = window.localStorage
-  var request = {}
-  for (var i = 0, len = storage.length; i < len; i++) {
-    let key = storage.key(i)
-    // console.log(key)
-    try{
-      request[key] = JSON.parse(dataGet(key))
-    }catch (e){
-      request[key] = dataGet(key)
-    }
-  }
-  var back = {}
-  for(let o in request){
-    let value = request[o],voType
-    try{
-      voType = request[o].constructor.name
-    }catch (e){
-      voType = ''
-    }
-    if(voType == 'Object'){
-      value = recursion(request[o],function(){
-        if (localStorage.getItem(o)) {
-          localStorage.setItem(o, JSON.stringify(back[o]))
-        }else if(sessionStorage.getItem(todo)){
-          sessionStorage.setItem(o, JSON.stringify(back[o]))
-        }
-      })
-    }
-    
-    Object.defineProperty(back, o, {
-      enumerable: true,
-      configurable: true,
-      get: function() {
-        return value
-      },
-      set: function(v) {
-        value = v
-        if (localStorage.getItem(o)) {
-          localStorage.setItem(o, JSON.stringify(v))
-        }else if(sessionStorage.getItem(todo)){
-          sessionStorage.setItem(o, JSON.stringify(v))
-        }
-      }
-    })
-  }
-  return back
 }
 export default Toast;
